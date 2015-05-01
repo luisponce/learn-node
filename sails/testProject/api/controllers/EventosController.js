724d3a1da9ca0afdf8462226daad0838720f4e1b
luis M Ponce <luisponcekun@gmail.com>
2015-05-01 15:05:06 -0500

    added sails testProject

---
+++ sails/testProject/api/controllers/EventosController.js
@@ -0,0 +1,11 @@
+/**
+ * EventosController
+ *
+ * @description :: Server-side logic for managing eventos
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ sails/testProject/api/controllers/RutasController.js
@@ -0,0 +1,11 @@
+/**
+ * RutasController
+ *
+ * @description :: Server-side logic for managing rutas
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
